344998240fca9dd5a3557b25faae0f8f96d908c2
David <davidiainsmall@gmail.com>
2019-05-04 15:58:18 +0100

    Custom control initial work.

---
+++ BattleBois/DivisionControl.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace BattleBois
+{
+    partial class DivisionControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DivisionControl));
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.AtkIcon = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.AtkIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(4, 4);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(96, 16);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Division Name";
+            // 
+            // AtkIcon
+            // 
+            this.AtkIcon.Image = ((System.Drawing.Image)(resources.GetObject("AtkIcon.Image")));
+            this.AtkIcon.Location = new System.Drawing.Point(7, 23);
+            this.AtkIcon.Name = "AtkIcon";
+            this.AtkIcon.Size = new System.Drawing.Size(16, 16);
+            this.AtkIcon.TabIndex = 1;
+            this.AtkIcon.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(72, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DivisionControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.AtkIcon);
+            this.Controls.Add(this.NameLabel);
+            this.Name = "DivisionControl";
+            this.Size = new System.Drawing.Size(196, 46);
+            ((System.ComponentModel.ISupportInitialize)(this.AtkIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.PictureBox AtkIcon;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
